74e8c75b0a901fd56f08cb8c1d38538902380c16
heidi <heidi.poels@tuni.fi>
2020-08-15 11:31:10 +0300

    Style sheet created. Removed browser style. Add thought to list as list item. Check box added. Empty text field after adding

--- todo.js
+++ todo.js
@@ -1,6 +1,9 @@
 function addTodo() {
     // Dollar sign(jQuery): findstuff in html.
     const thoughts = $('#mind').val();
-    $('#todoList').append('<li>' + thoughts + '</li>');
+    // Add thoughts to list as a list item with a checkbox
+    $('#todoList').append('<li><input type="checkbox">' + thoughts + '</li>');
+    // Empty text field
+    $('#mind').val('');
 }
 
